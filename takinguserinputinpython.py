9fd64b2543b88b9533bcdde65fdcbcf17d989ce1
anask601 <anask60155@gmail.com>
2023-01-31 23:51:26 +0530

    learn taking user input in py

---
+++ takinguserinputinpython.py
@@ -0,0 +1,16 @@
+# example 1
+a = input()
+print("my name is " + a)
+# example 2
+a = input()
+print("my name is", a)
+# example 3
+a = input('Enter your name: ')
+print("my name is", a)
+# example 4
+# input take and convert to string than concat that
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+print(x + y)
+# if want convert to number then we can use below functions
+print(int(x) + int(y))
