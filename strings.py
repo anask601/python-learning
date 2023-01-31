5fbf7056aa35fe965566e111f5efc3a94ac83db5
anask601 <anask60155@gmail.com>
2023-02-01 00:35:12 +0530

    strings in py

---
+++ strings.py
@@ -0,0 +1,20 @@
+firstName = 'anas'
+lastName = 'khan'
+fullName = "My name is, 'Khan Mohammad Anas'"
+# We can print in mutliple lines by using double quotes or single quotes
+fullNameMulyipleLine = """My name is, 
+yo working multiple line
+'Khan Mohammad Anas'"""
+print(fullName)
+print(fullNameMulyipleLine)
+# sring is like array, but not array
+# example 
+print(firstName[0])
+print(firstName[1])
+print(firstName[2])
+print(firstName[3])
+# print(firstName[5]) Throw arror of index
+
+
+for char in lastName:
+    print(char)
