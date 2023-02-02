6e9ee060408891a3c726e685ab7fc9c62a2e55fc
anask601 <anask60155@gmail.com>
2023-02-02 21:22:55 +0530

    string slice in py

---
+++ stringslicing.py
@@ -0,0 +1,10 @@
+name = "Anas Khan"
+mangolen = "mango"
+print(len(mangolen))
+print(name[0:4]) # incuding 0 but not 4
+print(name[1:4]) # incuding 1 but not 4
+print(name[:5])
+print(name[0:-3])
+print(name[-1:-3])
+print(name[0:len(mangolen) - 4])
+
