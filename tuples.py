1501e992e176a72da285deabdd7c50df16fcbe9a
anask601 <anask60155@gmail.com>
2023-02-20 23:17:56 +0530

    🎨 Learn tuples in py

---
+++ tuples.py
@@ -0,0 +1,15 @@
+# https://replit.com/@codewithharry/24-Day24-Introduction-to-Tuples#.tutorial/01-python_tuples.md
+# https://replit.com/@codewithharry/24-Day24-Introduction-to-Tuples#.tutorial/02-tuple_indexes.md
+tup = (1, 2, 76, 342, 32, "green", True)
+# tup[0] = 90
+print(type(tup), tup)
+print(len(tup))
+print(tup[0])
+print(tup[-1])
+print(tup[2])
+# print(tup[34])
+
+if  3421 in tup:
+  print("Yes 342 is present in this tuple")
+tup2 = tup[1:4]
+print(tup2)
