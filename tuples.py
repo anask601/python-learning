6e3f34f1fe11be1e49577a3e288c35a27ea9cbd4
anask601 <anask60155@gmail.com>
2023-02-20 23:24:49 +0530

    💡 Added more examples with proper comments

--- tuples.py
+++ tuples.py
@@ -9,6 +9,34 @@ print(tup[-1])
 print(tup[2])
 # print(tup[34])
 
+# create a tuple with a single element by including a trailing comma:
+my_tuple = (1,)
+print(my_tuple) # Output: (1,)
+
+# create a tuple without using parentheses, by separating the elements with commas:
+my_tuple = 1, 2, 3
+print(my_tuple) # Output: (1, 2, 3)
+
+# unpack a tuple into multiple variables:
+my_tuple = (1, 2, 3)
+a, b, c = my_tuple
+print(a) # Output: 1
+print(b) # Output: 2
+print(c) # Output: 3
+
+# use tuples to return multiple values from a function:
+def get_name_and_age():
+    name = "Alice"
+    age = 30
+    return name, age
+
+result = get_name_and_age()
+print(result) # Output: ("Alice", 30)
+name, age = get_name_and_age()
+print(name) # Output: "Alice"
+print(age) # Output: 30
+
+
 if  3421 in tup:
   print("Yes 342 is present in this tuple")
 tup2 = tup[1:4]
